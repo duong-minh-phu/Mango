cb5760825c5f2ed7ce8a5d2a68b272babebc2cc0
duong-minh-phu <134716556+duong-minh-phu@users.noreply.github.com>
2024-09-01 19:52:55 +0700

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/MangoController.cs
@@ -0,0 +1,54 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Model;
+using Mango.Services.CouponAPI.Model.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MangoController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+        public MangoController(AppDbContext db)
+        {
+            _db = db;
+            _response = new ResponseDto();
+        }
+        [HttpGet]
+        public object Get() {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = objList;
+                _response.Message = "succesfull";
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u=>u.CouponId==id);
+                _response.Result = objList;
+                _response.Message = "succesfull";
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Model/DTO/CouponDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponAPI.Model.DTO
+{
+    public class CouponDTO
+    {
+        public int CouponId { get; set; }
+        public string CouponName { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+        
+    }
+}
