2522a7781a3cae15538e691b4003bd5d04e89c9a
duong-minh-phu <134716556+duong-minh-phu@users.noreply.github.com>
2024-10-19 21:36:22 +0700

    update FE

--- Mango.Services.CouponAPI/Model/DTO/CouponDTO.cs
+++ Mango.Services.CouponAPI/Model/DTO/CouponDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace Mango.Services.CouponAPI.Model.DTO
+﻿namespace Mango.Web.Model
 {
     public class CouponDTO
     {
---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Model;
+using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllCouponsAsync();
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> CreateCouponsAsync(CouponDTO couponDto);
+        Task<ResponseDto?> UpdateCouponsAsync(CouponDTO couponDto);
+        Task<ResponseDto?> DeleteCouponsAsync(int id);
+    }
+}
